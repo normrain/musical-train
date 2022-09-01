3d3688b530742730cd24dcf9dd32146d7f57d73a
normrain <96089215+normrain@users.noreply.github.com>
2022-09-01 20:54:55 +0300

    Add files via upload

---
+++ DiningReview/src/main/java/com/myproject/DiningReview/repository/RestaurantRepository.java
@@ -0,0 +1,14 @@
+package com.myproject.DiningReview.repository;
+
+import com.myproject.DiningReview.model.Restaurant;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
+    Optional<Restaurant> findRestaurantsByNameAndZipCode(String name, String zipCode);
+    List<Restaurant> findRestaurantsByZipCodeAndPeanutScoreNotNullOrderByPeanutScore(String zipcode);
+    List<Restaurant> findRestaurantsByZipCodeAndDairyScoreNotNullOrderByDairyScore(String zipcode);
+    List<Restaurant> findRestaurantsByZipCodeAndEggScoreNotNullOrderByEggScore(String zipcode);
+}
